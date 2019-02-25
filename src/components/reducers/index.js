dc085e3c24a95d914154b234cc82ef64e454f3d0
MUIR, Sam <Sam.Muir@tower.co.nz>
2019-02-25 17:11:20 +1300

    Committing some Redux changes

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,8 +1,26 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
+// The below rootReducer breaks the main Redux principle - immutability. The State cannot change in place. 
+
+/* function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE){
+    state.articles.push(action.payload);
+  }
+  return state;
+}; */
+
+// In the below rootReducer, the initial State is left untouched. 
+
 function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE){
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
   return state;
 };
 
