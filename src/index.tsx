92a554c78aa98944fa50fdf294014775d18c3f0a
MUIR, Sam <Sam.Muir@tower.co.nz>
2019-03-07 15:32:10 +1300

    Continuing with adding TypeScript.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 //src/index.tsx
 
 import * as React from 'react';
-import { render } from 'react-dom';
-import App from "./components/App";
+import * as ReactDOM from 'react-dom';
+import App from "./ts/components/App";
 
-render(
-  <App />,
+const rootElement = document.getElementById("app");
 
-  document.getElementById("app")
-);
+// React.Component<any, any> is a part of the TypeScript typing definition for React. 
+// This is saying that the React.Component will allow the type 'any' to be used for the components state and props objects. This is similar to generics in C#
+class Main extends React.Component<any, any> {
+
+  // The constructor method is used to initialize the class. Here, super(props) is called which calls the constructor method on the parent class 'React.Component' - this passes any props that were passed into our constructor. 
+  constructor(props: any){
+    super(props);
+  }
+
+  // The render method is where the DOM for our component is defined. Your JSX is added inside the return() statement.
+  public render() {
+    return(
+      <div>
+        <App />
+      </div>
+    )
+  }
+}
+
+// Passes Main component to the defined Root Element variable that we output the component to.
+ReactDOM.render(<Main />, rootElement);
--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
 
-//import { List } from "./dumb/List/List";
-
-
-
+import List from "./dumb/List/List";
 
 const App = () => (
   <div className="row mt-5">
       <div className="col-md-4 offset-md-1">
       <h2>Articles</h2>
-    
+        <List />
       </div>
   </div>
 );
